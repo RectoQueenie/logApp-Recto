2d0ec5691898c24de92d05f56e5663b8fa6e9ae8
RectoQueenie <201980119@psu.palawan.edu.ph>
2022-03-17 08:09:12 +0800

    Added files from Log App Octo

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/Carlo0417">Carlo0417</a> for BSIT 3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
